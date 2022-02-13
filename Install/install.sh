9c98a38d42b42cd51be8b2f38f4c25a704ef84cd
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2022-02-13 14:37:27 +0000

    Created install scripts

--- Install/install.sh
+++ Install/install.sh
@@ -1,1 +1,9 @@
- 
+#!/bin/bash
+target_dir=~/.local/share/kservices5/srvrupload
+rm -Rf $target_dir
+mkdir $target_dir
+cd $target_dir/
+wget https://raw.githubusercontent.com/Chopper1337/srvrupload_dolphin/main/srvrupload.desktop
+wget https://raw.githubusercontent.com/Chopper1337/srvrupload_dolphin/main/srvrupload.sh
+chmod +x $target_dir/*
+exit
